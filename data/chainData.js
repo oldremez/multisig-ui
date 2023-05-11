0220dc36be5ecd0e8e35751c01627e119947359f
oldremez <83083413+oldremez@users.noreply.github.com>
2023-05-12 01:05:47 +0300

    ibc denom for neutron

--- data/chainData.js
+++ data/chainData.js
@@ -381,10 +381,10 @@ export const chainData = [
     },
     {
         chain_id: "neutron-1",
-        denom: "untrn",
-        displayDenom: "ntrn",
+        denom: "ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9",
+        displayDenom: "atom",
         base_denom: {
-            "denom": "untrn",
+            "denom": "ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9",
             "exponent": 6
         },
         name: "neutron",
